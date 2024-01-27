a2c6e09423d837e6c6b7e44955803e73ee967de4
FirstName LastName <youremail@provider.com>
2024-01-27 17:14:17 +0000

    inital folder set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.scss';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+      <Routes>
+
+        
+      </Routes>
       
     </div>
+    </BrowserRouter>
   );
 }
 
